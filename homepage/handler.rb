2d3917bb53436c6a427a8ab06cab47120085a820
Rogier <rogier@blijblijblij.com>
2021-01-05 09:54:29 +0100

    Add kubeclient gem

--- homepage/handler.rb
+++ homepage/handler.rb
@@ -2,7 +2,7 @@ class Handler
   def run(body, headers)
     status_code = 200 # Optional status code, defaults to 200
     response_headers = {"content-type" => "text/plain"}
-    body = "Hello world from the Ruby template"
+    body = "Hello world from the Ruby template #{Time.now}"
 
     return body, response_headers, status_code
   end
