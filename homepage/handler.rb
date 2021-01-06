3d5401abae4a7bdfde4e5c98ab7bab197825ad43
Rogier <rogier@blijblijblij.com>
2021-01-06 13:00:14 +0100

    Add gem for testing 2.7.2

--- homepage/handler.rb
+++ homepage/handler.rb
@@ -1,9 +1,13 @@
-class Handler
-  def run(body, headers)
-    status_code = 200 # Optional status code, defaults to 200
-    response_headers = {"content-type" => "text/plain"}
-    body = "Hello world from the Ruby template #{Time.now}"
+require 'figlet'
 
-    return body, response_headers, status_code
+class Handler
+  def run(body, _headers)
+    status_code = 200 # Optional status code, defaults to 200
+    response_headers = { 'content-type' => 'text/plain' }
+    font = Figlet::Font.new('big.flf')
+    figlet = Figlet::Typesetter.new(font)
+    body = "Hello world from the Ruby template #{Time.now} \n"
+    body << "#{figlet['hello world']} from the figlet gem"
+    [body, response_headers, status_code]
   end
 end
