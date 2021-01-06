8f8c4aad5abe3484ce1be103be634597864abf9a
Rogier <rogier@blijblijblij.com>
2021-01-06 13:17:56 +0100

    Log from logging gem

--- homepage/handler.rb
+++ homepage/handler.rb
@@ -4,10 +4,14 @@ class Handler
   def run(body, _headers)
     status_code = 200 # Optional status code, defaults to 200
     response_headers = { 'content-type' => 'text/plain' }
-    font = Figlet::Font.new('big.flf')
-    figlet = Figlet::Typesetter.new(font)
+
+    logging = Logging.logger(STDOUT)
+    logging.info('#############################')
+    logging.info 'Some logging from the installed logging gem'
+    logging.info('#############################')
+
     body = "Hello world from the Ruby template #{Time.now} \n"
-    body << "#{figlet['hello world']} from the figlet gem"
+
     [body, response_headers, status_code]
   end
 end
